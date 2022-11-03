cc06cfe0c556158f4cba00510602b1895dd8e597
nhatkhanh224 <55695197+nhatkhanhl@users.github.com>
2022-11-03 22:13:13 +0700

    CRUD Product

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,87 @@
+const Resize = require("../../root/Resize");
+const Product = require("../models/Product");
+const Category = require("../models/Category");
+const path = require('path');
+class ProductController {
+  async show(req, res) {
+    const categories = await Product.query()
+      .select("*")
+      .whereNull("deleted_at");
+    res.render("admin/product/show", {
+      layout: "layouts/admin",
+      categories: categories,
+    });
+  }
+  async add(req, res) {
+    const categories = await Category.query().select("*").whereNull('deleted_at');
+    res.render("admin/product/add", { layout: "layouts/admin",categories: categories});
+  }
+  async postProduct(req, res) {
+    // const imagePath = path.resolve('src/public/images');
+    // // call class Resize
+    // const fileUpload = new Resize(imagePath);
+    // if (!req.file) {
+    //   res.status(401).json({ error: "Please provide an image" });
+    // }
+    try {
+      await Product.query()
+        .insert({
+          name: req.body.name,
+          category_id: req.body.category_id,
+          description: req.body.description,
+          price: req.body.price,
+          // thumbnail: await fileUpload.save(req.file.buffer) 
+          thumbnail: req.body.thumbnail
+        })
+        .then(() => {
+          res.redirect("/product");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async edit(req, res) {
+    const categories = await Category.query().select("*").whereNull('deleted_at');
+    const product = await Product.query().findById(req.params.id);
+    res.render("admin/product/edit", {
+      layout: "layouts/admin",
+      categories: categories,
+      product:product
+    });
+  }
+  async updateProduct(req, res) {
+    const product_id = req.params.id;
+    try {
+      await Product.query()
+        .findById(product_id)
+        .patch({
+          name: req.body.name,
+          category_id: Number(req.body.category_id),
+          description: req.body.description,
+          price: Number(req.body.price),
+          thumbnail: req.body.thumbnail
+        })
+        .then(() => {
+          res.redirect("/product");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteProduct(req, res) {
+    const product_id = req.params.id;
+    try {
+      await Product.query()
+        .findById(product_id)
+        .patch({
+          deleted_at: new Date(),
+        })
+        .then(() => {
+          res.redirect("/product");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+module.exports = new ProductController();
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const AdminController = require('../app/controllers/AdminController');
 const CategoryController = require('../app/controllers/CategoryController');
+const ProductController = require('../app/controllers/ProductController');
+const upload = require('../app/middleware/uploadMiddleware');
 const router=express.Router();
+
+
 router.get('/',AdminController.index);
+
+//Category
 router.get('/category',CategoryController.show);
 router.get('/category-add',CategoryController.add);
 router.post('/category-add',CategoryController.postCategory);
 router.get('/category-edit-:id',CategoryController.edit);
 router.post('/category-update/:id',CategoryController.updateCategory);
 router.post('/category-delete/:id',CategoryController.deleteCategory);
+
+//Product
+router.get('/product',ProductController.show);
+router.get('/product-add',ProductController.add);
+router.post('/product-add',upload.single('thumbnail'),ProductController.postProduct);
+router.get('/product-edit-:id',ProductController.edit);
+router.post('/product-update/:id',ProductController.updateProduct);
+router.post('/product-delete/:id',ProductController.deleteProduct);
 module.exports = router;
